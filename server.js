314b85fabdb2ed106fb0c26a87ffabd1b5c746f4
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2022-11-09 21:15:02 +0500

    Ерофеев Александр 5/5 (Сливзерин)

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
-
     console.log(`server is listening on ${port}`);
 });
 
@@ -40,9 +39,20 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
     const pageBody = constructPageBody(reqBody);
-
+    console.log("1233");
     console.log(request.body);
 
-    response.send(`${pageHead}${pageBody}${pageFoot}`);
+    if (reqBody.petType != "cat" && reqBody.petType != "dog" && reqBody.petType != "tiger")
+        response.send("Неверный тип питомца");
+    if (reqBody.gender != "boy" && reqBody.petType != "girl")
+        response.send("Неверный пол питомца");
+    if (reqBody.tailLength < 7 || reqBody.tailLength > 120)
+        response.send("Неверная длина хвоста питомца");
+    if (reqBody.name.length < 3 || reqBody.name.length > 50)
+        response.send("Неверное имя питомца");
+    if (reqBody.rules == false)
+        response.send("Требуется согласие с условиями");
+    else
+        response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
 
