be2297dfe02927068ed068a6538d0da1733611ba
Denis-GM <gainutdinov.deniz@yandex.ru>
2022-11-10 00:28:52 +0500

    5/5

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 const port = 3000;
 const constructPageBody = require('./checkOrder');
 
-const { body,validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
-
-
 const pageHead =
 `
 <!DOCTYPE html>
@@ -42,13 +38,94 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
-    const pageBody = constructPageBody(reqBody);
+    // const pageBody = constructPageBody(reqBody);
 
-    console.log(request.body);
+    const validated = dataValidation(reqBody);
+    // console.log(Object.keys(validated).length);
+    constructValidPageBody(validated);
+    const pageBody =
+        Object.keys(validated).length == 0 ? constructPageBody(reqBody): constructValidPageBody(validated);
 
+    // console.log(pageBody);
+    console.log(reqBody);
+    dataValidation(reqBody);
+    // console.log(validated.petType);
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
 
-// body('name', 'Empty name').isLength({ min: 3, max: 50 });
-// body('age', 'Invalid age').optional({ checkFalsy: true }).isISO8601(),
+function dataValidation(requestBody) {
+    const conditions = {
+        petType: (value) => {
+            let arr = ['cat', 'dog', 'tiger'];
+            return arr.indexOf(value) != -1;
+        },
+
+        gender: (value) => {
+            let arr = ['boy', 'girl', 'none'];
+            return arr.indexOf(value) != -1;
+        },
+
+        eyeColor: (value) => {
+            return true;
+        },
+
+        name: (value) => {
+            const len = value.length;
+            return 3 <= len && len <= 50;
+        },
+
+        tailLength: (value) => {
+            if (isNaN(Number(value))) return false;
+            value = Number(value);
+            return 7 <= value && value <= 120;
+        },
+
+        dateOfBirth: (value) => {
+            let today = new Date().getFullYear();
+            age =today - ++value.split('-')[0];
+            // console.log(today);
+            // console.log(value.split('-')[0]);
+            return (5 < age && age < 110) ? true : false;
+
+            return true;
+        },
+
+        email: (value) => {
+            return true;
+        },
+
+        phone: (value) => {
+            return true;
+        },
+
+        rules: (value) => {
+            return value === 'true';
+        }
+    };
+    const wrongFields = {};
+    for (key in conditions) {
+        console.log(key);
+        if (!conditions[key](requestBody[key])) 
+            wrongFields[key] = requestBody[key];
+    }
+    console.log(wrongFields);
+    return wrongFields;
+}
+
+function constructValidPageBody(wrongFields) {
+    // let form = document.querySelector('.main-form')
+    // console.log(form);
+    // for(el in wrongFields){
+    //     console.log(el);
+    //     let elementByAttribute = document.querySelector('.tailLength');
+    //     // document.getElementsByName('tailLength').classList.add('err');
+    //     console.log(elementByAttribute);
+    //     // form.elements.el
+    // }
+    let result = '';
+    for (let field in wrongFields) {
+        result += `<p>Данные, введенные в поле ${field}, некорректны.</p>`;
+    }
+    return result;
+}
 
