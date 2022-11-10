d3e7d4985e4a3220acb9d9b041dfa32667b9affd
Roflmax <113230933+Roflmax@users.noreply.github.com>
2022-11-10 09:37:39 +0500

    Бутаков Максим Сливзерин 5/5

--- server.js
+++ server.js
@@ -44,5 +44,18 @@ app.post('/pets/orders', (request, response) => {
     console.log(request.body);
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
+    if (reqBody.petType != "cat" && reqBody.petType != "dog" && reqBody.petType != "tiger")
+        response.send("Неверный тип питомца");
+    if (reqBody.gender != "boy" && reqBody.petType != "girl")
+        response.send("Неверный пол питомца");
+    if (reqBody.tailLength < 7 || reqBody.tailLength > 120)
+        response.send("Неверная длина хвоста питомца");
+    if (reqBody.name.length < 3 || reqBody.name.length > 50)
+        response.send("Неверное имя питомца");
+    if (reqBody.rules == false)
+        response.send("Требуется согласие с условиями");
+    else
+        response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
+
 
