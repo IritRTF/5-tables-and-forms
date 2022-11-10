96eac09b718807bf4f657f30014c7f8a1cbcd16b
Aidimir <91966323+Aidimir@users.noreply.github.com>
2022-11-10 13:25:08 +0500

    done 1-5

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const constructPageBody = require('./checkOrder');
 
 
 const pageHead =
-`
+    `
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -14,12 +14,13 @@ const pageHead =
 </head>
 <body>
 <main>
+<div class="overlay"></div>
 <h1>Результат заказа питомца</h1>
 `;
 
 const pageFoot =
-`
-<a href="/">⃪ Вернуться к форме заказа</a>
+    `
+<a href="/" class="go-back">Вернуться</a>
 </main>
 </body>
 </html>
@@ -38,11 +39,27 @@ app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/pets/orders', (request, response) => {
-    const reqBody = request.body;
-    const pageBody = constructPageBody(reqBody);
+    const pageBody = validate(request.body);
 
     console.log(request.body);
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
 
+function validate(reqBody) {
+    const NAMEREGEX = /^([a-zа-я]+[\s]*)*$/i;
+    const PHONEREGEX = /^((\+7)|8)[9]\d{9}$/;
+
+    if (!NAMEREGEX.test(reqBody.name))
+        return `<p class="error">Имя должно состоять только из букв и не начинаться с пробела!</p>`;
+
+    if (!PHONEREGEX.test(reqBody.phone))
+        return `<p class="error">Номер телефона должен быть в виде 89000000000 или +79000000000!</p>`;
+
+    const date = new Date(reqBody.dateOfBirth);
+    const dateNow = new Date();
+    if (date > dateNow)
+        return `<p class="error">Дата рождения не может быть позднее сегодняшнего дня!</p>`;
+
+    return constructPageBody(reqBody);
+}
