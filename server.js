d5f7ba58cc06192be6dcbc22608a26cfe10ce1c5
unknown <ilyagolubev2021@gmail.com>
2023-11-09 00:14:44 +0500

    Голубев Илья 5/5

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const constructPageBody = require('./checkOrder');
 
 
 const pageHead =
-`
+    `
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -18,7 +18,7 @@ const pageHead =
 `;
 
 const pageFoot =
-`
+    `
 <a href="/">⃪ Вернуться к форме заказа</a>
 </main>
 </body>
@@ -39,9 +39,49 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
-    const pageBody = constructPageBody(reqBody);
-
-    console.log(request.body);
+    const valid = validateErrors(reqBody);
+    const pageBody = valid === true ? constructPageBody(reqBody) : getErrors();
+    console.log(reqBody.body);
+    console.log(errors);
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
+
+let errors = {};
+
+let validate = {
+    petType: (value) => { return isString(value) ? ['cat', 'dog'].indexOf(value) != -1 : false; },
+    gender: (value) => { return isString(value) ? ['boy', 'girl'].indexOf(value) != -1 : false; },
+    tailLength: (value) => { return isString(value) ? 7 <= Number(value) && Number(value) <= 120 : false; },
+    name: (value) => { return isString(value) ? 3 <= value.length && value.length <= 50 : false; },
+    dateOfBirth: (value) => {
+        let date = new Date();
+        date = date.getTime();
+        return isString(value) ? date - new Date(value).getTime() >= 0 : false;
+    },
+    email: (value) => {
+        let email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/i
+        return isString(value) ? email.test(value) : false
+    },
+    phone: (value) => {
+        let phone = /^(\+?7|8)?9\d{9}$/
+        return isString(value) ? phone.test(value) : false
+    },
+    name: (value) => { return value === 'true'; },
+};
+
+function isString(value) { return typeof (value) === 'string'; }
+
+function validateErrors(reqBody) {
+    for (i in validate) {
+        if (!validate[i](reqBody[i])) errors[i] = reqBody[i];
+    }
+
+    return errors.length == 0;
+}
+
+function getErrors() {
+    let messageError = '';
+    for (let i in errors) { messageError += `<p>Значение <b>${errors[i]}</b> в поле <i>${i}</i> некорректно.</p>`; }
+    return messageError;
+}
