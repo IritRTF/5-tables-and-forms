d9997a62426f3303686a344993ad766646496cc6
unknown <ilyagolubev2021@gmail.com>
2023-11-09 17:38:09 +0500

    Голубев Илья 5/5

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const port = 3000;
 const constructPageBody = require('./checkOrder');
 
-
-const pageHead =
-    `
+const pageHead = `
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -17,8 +15,7 @@ const pageHead =
 <h1>Результат заказа питомца</h1>
 `;
 
-const pageFoot =
-    `
+const pageFoot = `
 <a href="/">⃪ Вернуться к форме заказа</a>
 </main>
 </body>
@@ -33,55 +30,85 @@ app.listen(port, (err) => {
     console.log(`server is listening on ${port}`);
 });
 
-
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
-    const valid = validateErrors(reqBody);
-    const pageBody = valid === true ? constructPageBody(reqBody) : getErrors();
-    console.log(reqBody.body);
-    console.log(errors);
+    const validated = validateInputs(reqBody);
+    const pageBody =
+        validated === true ? constructPageBody(reqBody) : constructValidatedPageBody(validated);
+
+    console.log(request.body);
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
 
-let errors = {};
-
-let validate = {
-    petType: (value) => { return isString(value) ? ['cat', 'dog'].indexOf(value) != -1 : false; },
-    gender: (value) => { return isString(value) ? ['boy', 'girl'].indexOf(value) != -1 : false; },
-    tailLength: (value) => { return isString(value) ? 7 <= Number(value) && Number(value) <= 120 : false; },
-    name: (value) => { return isString(value) ? 3 <= value.length && value.length <= 50 : false; },
-    dateOfBirth: (value) => {
-        let date = new Date();
-        date = date.getTime();
-        return isString(value) ? date - new Date(value).getTime() >= 0 : false;
-    },
-    email: (value) => {
-        let email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/i
-        return isString(value) ? email.test(value) : false
-    },
-    phone: (value) => {
-        let phone = /^(\+?7|8)?9\d{9}$/
-        return isString(value) ? phone.test(value) : false
-    },
-    name: (value) => { return value === 'true'; },
+const naming = {
+    petType: 'Тип питомца',
+    gender: 'Пол',
+    tailLength: 'Длина хвоста',
+    name: 'Имя',
+    eyeColor: 'Цвет глаз',
+    dateOfBirth: 'Дата рождения',
+    email: 'Email',
+    phone: 'Телефон',
+    rules: 'Соглашение',
 };
 
-function isString(value) { return typeof (value) === 'string'; }
+function validateInputs(requestBody) {
+    const validators = {
+        petType: (value) => {
+            if (typeof value !== 'string') return false;
+            return (['cat', 'dog'].filter((el) => el === value).length !== 0);
+        },
+        gender: (value) => {
+            if (typeof value !== 'string') return false;
+            return ['boy', 'girl'].filter((el) => el === value).length !== 0;
+        },
+        tailLength: (value) => {
+            if (typeof value !== 'string') return false;
+            if (isNaN(Number(value))) return false;
+            return 7 <= Number(value) && Number(value) <= 120;
+        },
+        name: (value) => {
+            if (typeof value !== 'string') return false;
+            return 3 <= value.length && value.length <= 30;
+        },
+        eyeColor: (value) => {
+            if (typeof value !== 'string') return false;
+            return value.match(/#?[0-9A-Fa-f]{6}/g);
+        },
+        dateOfBirth: (value) => {
+            if (typeof value !== 'string') return false;
+            return value.match(/^((0[1-9]|1[012]|[1-9])[- /.](0[1-9]|[12][0-9]|3[01]|[1-9])[- /.](19|20)\d\d|(19|20)\d\d[- /.](0[1-9]|1[012]|[1-9])[- /.](0[1-9]|[12][0-9]|3[01]|[1-9]))$/g);
+        },
+        email: (value) => {
+            if (typeof value !== 'string') return false;
+            return value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
+        },
+        phone: (value) => {
+            if (typeof value !== 'string') return false;
+            return value.match(/^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/);
+        },
+        rules: (value) => {
+            if (typeof value !== 'string') return false;
+            return value === 'true';
+        },
+    };
 
-function validateErrors(reqBody) {
-    for (i in validate) {
-        if (!validate[i](reqBody[i])) errors[i] = reqBody[i];
+    const errors = {};
+    for (i in val) {
+        if (!val[i](requestBody[key]))
+            errors[i] = requestBody[i];
     }
-
-    return errors.length == 0;
+    return Object.entries(errors).length !== 0 ? errors : true;
 }
 
-function getErrors() {
-    let messageError = '';
-    for (let i in errors) { messageError += `<p>Значение <b>${errors[i]}</b> в поле <i>${i}</i> некорректно.</p>`; }
-    return messageError;
+function constructValidatedPageBody(errors) {
+    let result = '';
+    for (let field in errors) {
+        result += `<p>Значение <b>${errors[field]}</b> в поле <i>${naming[field]}</i> некорректно.</p>`;
+    }
+    return result;
 }
