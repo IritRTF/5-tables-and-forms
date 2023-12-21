b7c5dab5cbe86b2dd269fc9922ddd60a8d447b8a
kayaal34 <117476621+kayaal34@users.noreply.github.com>
2023-12-22 03:25:36 +0500

    Add files via upload

---
+++ 5-tables-and-forms-master/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const constructPageBody = require('./checkOrder');
+
+
+const pageHead =
+`
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<title>Результат заказа питомца</title>
+<link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+<main>
+<h1>Результат заказа питомца</h1>
+`;
+
+const pageFoot =
+`
+<a href="/">⃪ Вернуться к форме заказа</a>
+</main>
+</body>
+</html>
+`;
+
+
+app.post('/pets/orders', (request, response) => {
+    const reqBody = request.body;
+
+   
+    if (!reqBody.name || !reqBody.petType || !reqBody.age || !reqBody.gender || !reqBody.color || !reqBody.description) {
+        return response.send(`${pageHead}<p class="error">Ошибка: Все поля должны быть заполнены</p>${pageFoot}`);
+    }
+
+    const pageBody = constructPageBody(reqBody);
+
+    console.log(request.body);
+
+    response.send(`${pageHead}${pageBody}${pageFoot}`);
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+
+    console.log(`server is listening on ${port}`);
+});
+
+
+app.use(express.static('static'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/pets/orders', (request, response) => {
+    const reqBody = request.body;
+    const pageBody = constructPageBody(reqBody);
+
+    console.log(request.body);
+
+    response.send(`${pageHead}${pageBody}${pageFoot}`);
+});
+
